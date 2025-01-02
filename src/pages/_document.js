6692c570c15a6fc5f1442dfdeb723dd61be68a46
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2025-01-02 12:10:35 +0530

    Update GA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,9 +6,8 @@ export default function Document() {
       <Head />
       <body>
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KSL3Q9F6"
-          height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe>
-          </noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KSL3Q9F6" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
